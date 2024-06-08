6581c5f2d1ea57a999400d1bd527ed0b601b0483
HAMZA-HED-MESSAOUD <hamza.h@edraak.io>
2024-06-08 20:06:07 +0100

    first commit project setup frontend with nextjs and backend with django

---
+++ backend/backend/urls.py
@@ -0,0 +1,12 @@
+from contrib.views import char_count
+from django.contrib import admin
+from django.urls import path, re_path, include
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('example/', include('testApp.api.urls')),
+    path("char_count", char_count, name="char_count"),
+    # re_path(".*", TemplateView.as_view(template_name="index.html")),
+
+]
